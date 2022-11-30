b2cd5e9d1617e4e7edf0772f5fea5b3d5a1b0320
nabiloo6060 <118072654+nabiloo6060@users.noreply.github.com>
2022-11-30 10:14:37 +0330

    Add files via upload

---
+++ for2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+main() {
+	int a,i,c;
+	cout<<"please enter a number:";
+	cin>>a;
+	 
+	 c=0;
+	 for(i=1;   i<=a;  i++){
+	 	if(a%i==0);
+	 	c+=i;
+	 }
+	 cout<<c;
+	
+	
+	
+	
+	
+	
+	
+	
+}
